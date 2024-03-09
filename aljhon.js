aa8cfa6813536b781cb5f497cb03047c1550dd36
Zaragosa <n.zaragosa00113@student.tsu.edu.ph>
2024-03-09 15:18:05 +0800

    inayos

---
+++ aljhon.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed(".text", {
+        strings: ["BSIT Student", "Motorcycle Enthusiast", "3rd Year Student", "PC Builder"],
+        typeSpeed: 200,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
