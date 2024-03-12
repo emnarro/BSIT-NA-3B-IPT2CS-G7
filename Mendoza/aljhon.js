0dc8f45f2fa1564e5d74968a0ab59829c6aa3e82
ajmendoza-idk <159223492+ajmendoza-idk@users.noreply.github.com>
2024-03-12 18:57:50 +0800

    Delete Mendoza directory

--- Mendoza/aljhon.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var typed = new Typed(".text", {
-        strings: ["BSIT STUDENT", "Motorcycle Enthusiast", "3rd Year Student", "PC Builder"],
-        typeSpeed: 200,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
-    });
-});
