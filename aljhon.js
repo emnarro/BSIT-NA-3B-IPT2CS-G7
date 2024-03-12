67ead61dfb84d38c9d0f700118b35de911c72efc
Al Jhon Mendoza <aj.mendoza01041@student.tsu.edu.ph>
2024-03-12 20:17:15 +0800

    pinale

--- aljhon.js
+++ aljhon.js
@@ -7,3 +7,41 @@ document.addEventListener('DOMContentLoaded', function () {
         loop: true
     });
 });
+
+let myDocument = document.documentElement;
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=>{
+    if(btn.textContent == "Go Fullscreen"){
+        if(myDocument.requestFullscreen){
+            myDocument.requestFullscreen();
+        }
+        else if(myDocument.msRequestFullscreen){
+            myDocument.msRequestFullscreen();
+        }
+        else if(myDocument.mozRequestFullscreen){
+            myDocument.mozRequestFullscreen();
+        }
+        else if(myDocument.webkitRequestFullscreen){
+            myDocument.webkitRequestFullscreen();
+        }
+
+        btn.textContent = "Exit Fullscreen";
+    }
+    else{
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        }
+        else if(document.msexitFullscreen){
+            document.msexitFullscreen();
+        }
+        else if(document.mozexitFullscreen){
+            document.mozexitFullscreen();
+        }
+        else if(document.webkitexitFullscreen){
+            document.webkitexitFullscreen();
+        }
+
+        btn.textContent = "Go Fullscreen";
+    }
+});
